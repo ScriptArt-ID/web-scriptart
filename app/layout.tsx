02f5c1d253f9d0635878762b6bb681c33b003c0d
K1ng-D <dhanicysec@gmail.com>
2025-02-15 15:05:42 +0700

    update febrian

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from '../components/Footer';
 import "./globals.css";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
